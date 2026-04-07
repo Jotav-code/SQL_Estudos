90ecc0796080030088a1b74a65492e7f0972d823
João Vitor Soares Bispo <joaovitor646260@Gmail.com>
2026-04-07 15:00:49 -0300

    Create discente table in universidade schema

---
+++ tabela.discente.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA universidade;
+	
+    CREATE TABLE universidade. discente(
+    	
+      	matricula NUMERIC(13),
+      	nome VARCHAR(20),
+      	ano_ingresso INT,
+      	periodo_de_ingresso INT,
+      	status_discente VARCHAR(20),
+      	nome_curso VARCHAR
+    	
+    );
