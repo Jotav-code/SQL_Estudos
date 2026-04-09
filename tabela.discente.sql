8a924526ccdf150fa3d69ed862795b7f91acbbd3
João Vitor Soares Bispo <joaovitor646260@Gmail.com>
2026-04-09 14:34:34 -0300

    Modify tabela.discente.sql to enhance table schema
    Updated the discente table structure and added new fields.

--- tabela.discente.sql
+++ tabela.discente.sql
@@ -1,12 +1,33 @@
+
+
 CREATE SCHEMA universidade;
+CREATE TYPE universidade.tipo_status_discente AS ENUM ('ATIVO', 'REGULAR', 'NAOESTA');
 	
-    CREATE TABLE universidade. discente(
-    	
-      	matricula NUMERIC(13),
-      	nome VARCHAR(20),
-      	ano_ingresso INT,
-      	periodo_de_ingresso INT,
-      	status_discente VARCHAR(20),
+    CREATE TABLE universidade.discente(
+    	matricula NUMERIC(13),
+      	nome VARCHAR(60),
+      	ano_ingresso SMALLINT NOT NULL,
+      	periodo_ingresso NUMERIC NOT NULL,
+      	status_discente universidade.tipo_status_discente,
       	nome_curso VARCHAR
-    	
     );
+   
+
+   
+
+INSERT INTO universidade.discente VALUES ('202400064307', 'joao', '2024', '1', 'ATIVO', 'engenharia de computação'), ('202400029110', 'esther kelre', '2024', '1', 'ATIVO', 'engenharia de computação'), (null, 'esther kelre', '2024', '1', 'ATIVO', 'engenharia de computação');
+
+
+
+ALTER TABLE universidade.discente 
+	ADD numero NUMERIC;
+    
+
+UPDATE universidade.discente
+	SET NUMERO = 7999606
+    WHERE nome = 'joao';
+    
+
+UPDATE universidade.discente
+	SET NUMERO = 7999606
+    WHERE nome = 'esther kelre';
